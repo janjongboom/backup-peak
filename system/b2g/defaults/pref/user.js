42ee50ac7d8ed941a5dd51262c63f511678b91c1
Jan Jongboom <janjongboom@gmail.com>
2015-02-11 12:20:13 +0100

    Peak backup directory

---
+++ system/b2g/defaults/pref/user.js
@@ -0,0 +1,26 @@
+pref('browser.manifestURL', "app://system.gaiamobile.org/manifest.webapp");
+pref('b2g.system_manifest_url', "app://system.gaiamobile.org/manifest.webapp");
+pref('b2g.neterror.url', "app://system.gaiamobile.org/net_error.html");
+pref('browser.homescreenURL', "app://system.gaiamobile.org/index.html");
+pref('b2g.system_startup_url', "app://system.gaiamobile.org/index.html");
+pref('network.http.max-connections-per-server', 15);
+pref('dom.mozInputMethod.enabled', true);
+pref('layout.css.sticky.enabled', true);
+pref('intl.uidirection.qps-plocm', "rtl");
+pref('ril.debugging.enabled', false);
+pref('dom.mms.version', 17);
+pref('b2g.wifi.allow_unsafe_wpa_eap', true);
+pref('network.dns.localDomains', "gaiamobile.org,findmydevice.gaiamobile.org,fl.gaiamobile.org,pdfjs.gaiamobile.org,keyboard.gaiamobile.org,wallpaper.gaiamobile.org,marketplace.firefox.com.gaiamobile.org,communications.gaiamobile.org,verticalhome.gaiamobile.org,sharedtest.gaiamobile.org,calendar.gaiamobile.org,clock.gaiamobile.org,callscreen.gaiamobile.org,bluetooth.gaiamobile.org,camera.gaiamobile.org,music.gaiamobile.org,homescreen.gaiamobile.org,settings.gaiamobile.org,ringtones.gaiamobile.org,search.gaiamobile.org,ftu.gaiamobile.org,email.gaiamobile.org,emergency-call.gaiamobile.org,gallery.gaiamobile.org,system.gaiamobile.org,operatorvariant.gaiamobile.org,collection.gaiamobile.org,wappush.gaiamobile.org,costcontrol.gaiamobile.org,bookmark.gaiamobile.org,fm.gaiamobile.org,browser.gaiamobile.org,sms.gaiamobile.org,video.gaiamobile.org,test-receiver-2.gaiamobile.org,test-ime.gaiamobile.org,test-sensors.gaiamobile.org,ds-test.gaiamobile.org,test-receiver-1.gaiamobile.org,test-receiver-inline.gaiamobile.org,mochitest.gaiamobile.org,contacts-ds-provider2.gaiamobile.org,membuster.gaiamobile.org,sheet-app-3.gaiamobile.org,test-findmydevice.gaiamobile.org,uitest.gaiamobile.org,crystalskull.gaiamobile.org,test-agent.gaiamobile.org,demo-keyboard.gaiamobile.org,uitest-privileged.gaiamobile.org,contacts-ds-provider1.gaiamobile.org,marketplace-dev.allizom.org.gaiamobile.org,marketplace.allizom.org.gaiamobile.org,homescreen-stingray.gaiamobile.org,geoloc.gaiamobile.org,share-receiver.gaiamobile.org,image-uploader.gaiamobile.org,test-container.gaiamobile.org,test-fxa-client.gaiamobile.org,in_app_pay_test.gaiamobile.org,test-keyboard-app.gaiamobile.org,test-iac-publisher.gaiamobile.org,cubevid.gaiamobile.org,test-wappush.gaiamobile.org,bookmarks-reader.gaiamobile.org,contacts-manager.gaiamobile.org,sheet-app-1.gaiamobile.org,test-iac-subscriber.gaiamobile.org,music2.gaiamobile.org,sheet-app-2.gaiamobile.org,testpermission.gaiamobile.org,test-otasp.gaiamobile.org,feedback.gaiamobile.org,template.gaiamobile.org");
+pref('devtools.debugger.prompt-connection', false);
+pref('devtools.debugger.forbid-certified-apps', false);
+pref('javascript.options.discardSystemSource', false);
+pref('b2g.adb.timeout', 0);
+
+pref("geo.gps.supl_server", "supl.izatcloud.net");
+pref("geo.gps.supl_port", 22024);
+pref("dom.payment.provider.0.name", "firefoxmarket");
+pref("dom.payment.provider.0.description", "marketplace.firefox.com");
+pref("dom.payment.provider.0.uri", "https://marketplace.firefox.com/mozpay/?req=");
+pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
+pref("dom.payment.provider.0.requestMethod", "GET");
+
